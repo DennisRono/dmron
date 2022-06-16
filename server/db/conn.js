fbb9b2d6293774f45bee9ec850fde2fb712c8373
DennisRono <finnneron1@gmail.com>
2022-06-16 19:23:01 +0300

    whale headed concerned open lower

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,18 +1,1 @@
-const MongoClient = require('mongodb').MongoClient;
-//Mongo Connection credentials
-const CONNECTION_URL = process.env.CONNECTION_URL
-const DATABASE_NAME = process.env.DATABASE_NAME
-
-let _db;
-module.exports = {
-    connectToServer: function (callback) {
-        MongoClient.connect(CONNECTION_URL, function (err, client) {
-            _db = client.db(DATABASE_NAME);
-            console.log('Connected to database: ' + DATABASE_NAME);
-            return callback(err);
-        });
-    },
-    getDb: function () {
-        return _db;
-    }
-};
+const mongoose = require('mongoose')
