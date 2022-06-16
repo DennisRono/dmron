c683136880ca4008c6115572ea38495ff897f582
DennisRono <finnneron1@gmail.com>
2022-06-16 17:43:01 +0300

    nor call rocket noise nature

--- server/server.js
+++ server/server.js
@@ -1,5 +1,19 @@
-const express = require('express');
-const PORT = process.env.PORT||8000;
-const app = express();
+const express = require('express')
+var cors = require('cors')
+const PORT = process.env.PORT||8000
+const app = express()
 
-app.listen(console.log(`server is running on port ${PORT}`))
+var whitelist = ['https://dmron.nullchemy.com', 'http://localhost:3000']
+var options = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(options))
+
+app.listen(PORT, () => {console.log(`server is running on port ${PORT}`)})
