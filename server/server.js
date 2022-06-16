69108d03830cd879c157e2ba16091f4445a8a934
DennisRono <finnneron1@gmail.com>
2022-06-16 19:17:01 +0300

    won explore nation table queen

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
 const PORT = process.env.PORT||8000
 const app = express()
 
