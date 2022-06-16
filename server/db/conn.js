c7856afbc71942d0e42555c9f285542e15503404
DennisRono <finnneron1@gmail.com>
2022-06-16 19:26:00 +0300

    page plan deep lesson asleep

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,1 +1,14 @@
 const mongoose = require('mongoose')
+
+const conn = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        })
+    } catch(err){
+        console.err(err)
+    }
+}
+
+module.exports = conn
