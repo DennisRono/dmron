ab8dad002e86fd440007c193cd2345634dc08a65
DennisRono <finnneron1@gmail.com>
2022-06-16 19:18:01 +0300

    door wore age rain shirt

---
+++ server/config/corsOptions.js
@@ -0,0 +1,10 @@
+var whitelist = ['https://dmron.nullchemy.com', 'http://localhost:3000']
+var options = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
