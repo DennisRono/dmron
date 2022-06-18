c20ae4b2277a7bd0f8ef40dd393f67b10dcd1bff
DennisRono <finnneron1@gmail.com>
2022-06-18 09:27:00 +0300

    wild frog maybe spread author

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', (req, res, next) => {
-    res.json(req)
+    res.json({msg:"hurray"})
 })
 
 module.exports = router
