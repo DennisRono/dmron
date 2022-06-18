f754b6a8891d88903c158604563fddcba447b717
DennisRono <finnneron1@gmail.com>
2022-06-18 08:49:02 +0300

    sight flame receive well behind

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
-import React, { Fragment } from 'react'
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import React, { Fragment, useEffect, useState } from 'react'
+import { io } from 'socket.io-client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './routes/Home'
 import About from './routes/About'
 import Blog from './routes/Blog'
 import Contact from './routes/Contact'
 
 const App = () => {
+  const [time, setTime] = useState('fetching')
+  useEffect(() => {
+    const socket = io('http://localhost:5000')
+    socket.on('connect', () => console.log(socket.id))
+    socket.on('connect_error', () => {
+      setTimeout(() => socket.connect(), 5000)
+    })
+    socket.on('time', (data) => setTime(data))
+    socket.on('disconnect', () => setTime('server disconnected'))
+  }, [])
   return (
     <Fragment>
       <BrowserRouter>
