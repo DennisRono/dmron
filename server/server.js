4bfef192e6161f88e28931313ca042de198cd85d
DennisRono <finnneron1@gmail.com>
2022-06-18 08:44:03 +0300

    older heading happened give leg

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const http = require('http');
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, {cors:{origin: 'http://localhost:3000'}})
 const PORT = process.env.PORT||8000
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -11,6 +15,10 @@ const options = require('./config/corsOptions')
 
 //connect to mongoDb
 conn()
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
 
 //routes
 const authRoute = require('./routes/auth');
@@ -26,5 +34,5 @@ app.use('/auth', authRoute);
 
 mongoose.connection.once('open', ()=>{
     console.log('Connected to MongoDB')
-    app.listen(PORT, () => {console.log(`server is running on port ${PORT}`)})
+    server.listen(PORT, () => {console.log(`server is running on port ${PORT}`)})
 })
