4780c6e46e9d8d2f3f89a5fcef0c152d6cb09c1c
DennisRono <finnneron1@gmail.com>
2022-06-18 09:26:03 +0300

    remarkable mission verb tonight tin

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/register', (req, res, next) => {
-    res.json({'request': req})
+    res.json(req)
 })
 
 module.exports = router
