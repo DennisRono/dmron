1cd4a30a3180af8ddf8fd88aecf8fb7e1bb41b9e
DennisRono <finnneron1@gmail.com>
2022-06-18 09:34:01 +0300

    mental melted poor spin fur

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+const { registerDataSchema, loginDataSchema, regDataSchema, logDataSchema } = require("../schemas/user");
 
 router.get('/', (req, res) => {
     res.json({msg:"Welcome to Dmron Authentication"})
@@ -9,8 +12,16 @@ router.post('/', (req, res) => {
     res.json({msg:"Welcome to Dmron Authentication"})
 })
 
-router.post('/register', (req, res, next) => {
-    res.json({msg:"hurray"})
+router.post('/register', async (req, res, next) => {
+    try {
+        const validate = await registerDataSchema.validateAsync(req.body);
+    } catch (error) {
+        if (error.isJoi === true) {
+            res.status(422).send(error.details[0].message);
+        } else {
+            res.status(500).send(error);
+        }
+    }
 })
 
 router.post('/login', (req, res, next) => {
