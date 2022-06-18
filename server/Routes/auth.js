eadcb038185ad8116623f4c07eee1823ddf62fbd
DennisRono <finnneron1@gmail.com>
2022-06-18 09:29:01 +0300

    tiny health winter means eat

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.json({msg:"Welcome to Dmron Authentication"})
+})
+
+router.post('/', (req, res) => {
+    res.json({msg:"Welcome to Dmron Authentication"})
+})
+
 router.post('/register', (req, res, next) => {
     res.json({msg:"hurray"})
 })
