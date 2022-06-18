a616a03820f676312dc3ed606cfcb21bff6c39d6
DennisRono <finnneron1@gmail.com>
2022-06-18 11:06:00 +0300

    earn joy improve operation husband

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -18,7 +18,7 @@ router.post('/register', async (req, res, next) => {
         const validate = await registerDataSchema.validateAsync(req.body);
         //check if user is already registered
         const User = user.exists({"email": validate.email});
-        if (User) return res.status(400).json({ msg: "User already registered" });
+        if (!User) return res.status(400).json({ msg: "User already registered" });
         //hash password
         bcrypt.genSalt(10, (err, salt) => {
             if (err) {
