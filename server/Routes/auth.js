eaccbc06bad9638f3897426c251e98c7374bdd9b
DennisRono <finnneron1@gmail.com>
2022-06-18 09:21:01 +0300

    hold vapor experiment fifth related

--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-module.exports = router;
+router.post("/register", async(req, res, next) => {
+
+}
+
+module.exports = router
